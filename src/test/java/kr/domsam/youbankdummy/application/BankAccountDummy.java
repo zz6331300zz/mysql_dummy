bbdab3029f3b373a961c831c6e6c801341d9c931
sbsteacher <id@gmail.com>
2025-11-21 17:45:30 +0900

    test - 3

--- src/test/java/kr/domsam/youbankdummy/application/BankAccountDummy.java
+++ src/test/java/kr/domsam/youbankdummy/application/BankAccountDummy.java
@@ -4,8 +4,11 @@ package kr.domsam.youbankdummy.application;
 import kr.domsam.youbankdummy.Dummy;
 import kr.domsam.youbankdummy.entity.BankAccount;
 import kr.domsam.youbankdummy.entity.Customer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,58 +16,79 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankAccountDummy extends Dummy {
 
     @Autowired CustomerRepository customerRepository;
     @Autowired BankAccountRepository bankAccountRepository;
+    List<Customer> customerList;
+
+    @BeforeAll
+    void beforeAll() {
+        customerList = customerRepository.findAll();
+    }
 
     @Test
+    @Rollback(false)
     void insOneByOne() {
-        List<Customer> customerList = customerRepository.findAll();
         for(Customer c : customerList) {
-            BankAccount ba = BankAccount.builder()
-                    .accountNo(c.getCustId())
-                    .customer(c)
-                    .accountPassword(faker.number().digits(6))
-                    .bankCode(faker.number().digits(3))             // 은행코드는 3자리 숫자로 예시
-                    .accountName(faker.finance().iban())            // 계좌명 → IBAN 등 랜덤 금융 문자열
-                    .depositorName(c.getCustName())        // 예금주명
-
-                    // 잔액
-                    .balance(new BigDecimal(faker.number().numberBetween(0, 999999999))) // 최대 9억
-
-                    // 날짜
-                    .establishedDate(randomDatePast())              // 개설일
-                    .maturityDate(randomDateFuture())               // 만기일
-                    .cancelDate(null)                               // 기본 NULL
-
-                    // 타입/코드
-                    .accountType(faker.options().option("01", "02", "03", "04")) // 계좌타입
-                    .limitAmount(BigDecimal.valueOf(faker.number().numberBetween(0, 10000000))) // 한도
-                    .manageBranchId(faker.number().digits(4))       // 4자리 지점 ID
-
-                    // 사용 여부
-                    .useYn("Y")
-                    .virtualAccountYn(faker.bool().bool() ? "Y" : "N")
-
-                    // 비고
-                    .remark(faker.lorem().sentence())
-
-                    // 등록 정보
-                    .regtGubun("3")
-                    .regtId("SYS")
-                    .regtDatetime(LocalDateTime.now())
-                    .mdfId(null)
-                    .mdfDatetime(null)
-                    .build();
-
-
+            BankAccount ba = generateBankAccount(c);
             bankAccountRepository.save(ba);
+            System.out.println(ba);
         }
         bankAccountRepository.flush();
+    }
 
+    BankAccount generateBankAccount(Customer c) {
+        return BankAccount.builder()
+                .accountNo(generateAccountNo())
+                .customer(c)
+                .accountPassword(faker.number().digits(6))
+                .bankCode(faker.number().digits(3))             // 은행코드는 3자리 숫자로 예시
+                .accountName(faker.finance().iban())            // 계좌명 → IBAN 등 랜덤 금융 문자열
+                .depositorName(c.getCustName())        // 예금주명
 
+                // 잔액
+                .balance(new BigDecimal(faker.number().numberBetween(0, 999999999))) // 최대 9억
+
+                // 날짜
+                .establishedDate(randomDatePast())              // 개설일
+                .maturityDate(randomDateFuture())               // 만기일
+                .cancelDate(null)                               // 기본 NULL
+
+                // 타입/코드
+                .accountType(faker.options().option("01", "02", "03", "04")) // 계좌타입
+                .limitAmount(BigDecimal.valueOf(faker.number().numberBetween(0, 10000000))) // 한도
+                .manageBranchId(faker.number().digits(4))       // 4자리 지점 ID
+
+                // 사용 여부
+                .useYn("Y")
+                .virtualAccountYn(faker.bool().bool() ? "Y" : "N")
+
+                // 비고
+                .remark(faker.lorem().sentence())
+
+                // 등록 정보
+                .regtGubun("3")
+                .regtId("SYS")
+                .regtDatetime(LocalDateTime.now())
+                .mdfId(null)
+                .mdfDatetime(null)
+                .build();
+    }
+
+    @Test
+    @Rollback(false)
+    void insAddAccount() {
+        final int SIZE = 100;
+
+        for(int i=0; i<SIZE; i++) {
+            Customer c = customerList.get(  faker.random().nextInt(customerList.size())  );
+            BankAccount ba = generateBankAccount(c);
+            bankAccountRepository.save(ba);
+            System.out.println(ba);
+        }
+        bankAccountRepository.flush();
     }
 
     // 계좌번호 (20자리 숫자)
